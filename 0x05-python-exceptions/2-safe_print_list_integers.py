392755c26d1dae6959ee20ec0c12c9a89f2ceca1
Justmaina <githuamaina10@gmail.com>
2023-10-23 14:59:21 -0700

    val

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             counter += 1
         except (ValueError, TypeError):
             pass
-        print()
-        Return
+    print("")
+    Return (counter)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except ZeroDivisionError:
+    except (TypeError,ZeroDivisionError):
         div = None
     finally:
         print("Inside result: {}".format(div))
-        Return div
+    return (div)
